28cacac4a9d8fcead788023935546f2f0a56f478
Yehor Bondarkov <99820593+bondarkoff@users.noreply.github.com>
2022-08-13 20:32:49 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,35 @@
+function onEntry(entry) {
+    entry.forEach(change => {
+      if (change.isIntersecting) {
+       change.target.classList.add('element-show');
+      }
+    });
+  }
+  
+  let options = {
+    threshold: [0.5] };
+  let observer = new IntersectionObserver(onEntry, options);
+  let elements = document.querySelectorAll('.element-animation');
+  
+  for (let elm of elements) {
+    observer.observe(elm);
+}
+
+const counters = document.querySelectorAll('.skills__ratings-counter'),
+      lines = document.querySelectorAll('.skills__ratings-line span');
+
+counters.forEach( (item, i) => {
+    lines[i].style.width = item.innerHTML;
+});
+
+const hamburger = document.querySelector('.hamburger'),
+      menu = document.querySelector('.sidemenu'),
+      closeElem = document.querySelector('.sidemenu__close');
+
+hamburger.addEventListener('click', () => {
+    menu.classList.add('active');
+});
+
+closeElem.addEventListener('click', () => {
+    menu.classList.remove('active');
+});
